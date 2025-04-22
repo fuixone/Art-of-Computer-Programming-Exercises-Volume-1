33d593246a8eb8516dcb557c0b3afb25679b0989
dani <fuixone@gmail.com>
2025-04-22 05:27:28 -0700

    improved eucledean algorithm

--- 1.1/e.c
+++ 1.1/e.c
@@ -2,49 +2,80 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #define DEBUGGER(a, b, expected, result) do { \
     printf("Input: %d, %d should equal %d. Equaled: %d\n", (a), (b), (expected), (result)); \
     assert((expected) == (result)); \
 } while (0)
 
-int EuclideanAlgorithm(int m, int n)
+typedef struct Input 
+{
+    int m;
+    int n;
+} Input;
+
+// If -1 -> continue algorithm execution
+// If any other number -> return that number
+int PreliminaryStep(struct Input* input)
+{
+    if(input->m < 0) input->m = -input->m;
+    if(input->n < 0) input->n = -input->n;
+
+    if(input->m == 0 && input->n == 0) return 0; 
+
+    if(input->m == 0) return input->n;
+    if(input->n == 0) return input->m;
+
+    if(input->m < input->n)
+    {
+        int tempM = input->m;
+        input->m = input->n;
+        input->n = tempM;
+    }
+
+    return -1;
+}
+
+int EuclideanAlgorithm(struct Input* input, int flag)
 {
 
-    if(m < 0) m = -m;
-    if(n < 0) n = -n;
-
-    if(m == 0 && n == 0) return 0;
-
-    if(m == 0) return n;
-    if(n == 0) return m;
+    if(flag) 
+    {
+        int prelimResult;
+        if((prelimResult = PreliminaryStep(input)) != -1) return prelimResult;
+        flag = 0;
+    }
 
     int remainder;
-    if((remainder = m % n) == 0) return n;
+    if((remainder = input->m % input->n) == 0) return input->n;
     
-    m = n;
-    n = remainder;
-    return EuclideanAlgorithm(m, n);
+    input->m = input->n;
+    input->n = remainder;
+    return EuclideanAlgorithm(input, 0);
 }
 
 int main()
 {
-    int result = EuclideanAlgorithm(48, 18);
+    int result = EuclideanAlgorithm(&(Input){48, 18}, 1);
     DEBUGGER(48, 18, 6, result);
 
-    result = EuclideanAlgorithm(101, 103);
+    result = EuclideanAlgorithm(&(Input){119, 544}, 1);
+    DEBUGGER(119, 544, 17, result);
+
+    result = EuclideanAlgorithm(&(Input){101, 103}, 1);
     DEBUGGER(101, 103, 1, result);
 
-    result = EuclideanAlgorithm(100, 25);
+    result = EuclideanAlgorithm(&(Input){100, 25}, 1);
     DEBUGGER(100, 25, 25, result);
 
-    result = EuclideanAlgorithm(0, 34);
+    result = EuclideanAlgorithm(&(Input){0, 34}, 1);
     DEBUGGER(0, 34, 34, result);
 
-    result = EuclideanAlgorithm(123456, 789012);
+    result = EuclideanAlgorithm(&(Input){123456, 789012}, 1);
     DEBUGGER(123456, 789012, 12, result);
 
-    result = EuclideanAlgorithm(-48, 18);
+    result = EuclideanAlgorithm(&(Input){-48, 18}, 1);
     DEBUGGER(-48, 18, 6, result);
 
     return 0;
